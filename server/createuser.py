ff5e11b7819bd785c4ae7893b30c29ae8a40f379
meghanlo <lo.meghan.e@gmail.com>
2022-02-18 20:03:27 -0500

    update createuser function

--- server/createuser.py
+++ server/createuser.py
@@ -8,25 +8,49 @@ db = client.TestData
 userCollection = db.Users
 
 def lambda_handler(event, context):
+
+    if event.body
+        let body = JSON.parse(event.body)
+        if body.userId:
+            userId = body.userId
+        else:
+            return {
+                'statusCode': 401,
+                'body': json.dumps('User cannot be blank')
+            }
+
+        if body.password:
+            password = body.password
+        else:
+            return {
+                'statusCode': 401,
+                'body': json.dumps('Password cannot be blank')
+            }
+    else
+        return {
+                'statusCode': 401,
+                'body': json.dumps('Request missing body')
+            }
+
     
-    userId = event['queryStringParameters']['userId'] or None
-    password = event['queryStringParameters']['password'] or None
+    # userId = event['queryStringParameters']['userId'] or None
+    # password = event['queryStringParameters']['password'] or None
     # userId  = request.args.get('userId', None)
     # password  = request.args.get('password', None)
 
     existingUser = userCollection.find_one({'userId':userId}) or None
     
-    if not userId:
-        return {
-            'statusCode': 401,
-            'body': json.dumps('User cannot be blank')
-        }
+    # if not userId:
+    #     return {
+    #         'statusCode': 401,
+    #         'body': json.dumps('User cannot be blank')
+    #     }
     
-    if not password:
-        return {
-            'statusCode': 401,
-            'body': json.dumps('Password cannot be blank')
-        }
+    # if not password:
+    #     return {
+    #         'statusCode': 401,
+    #         'body': json.dumps('Password cannot be blank')
+    #     }
 
     if existingUser:
         return {
